ad5b7e5cdccef5641abdf48a4ae62ce63168b751
Samuel Odundo <samieodd@gmail.com>
2020-06-06 13:28:01 +0300

    Created app logic and connected to test template

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
 """ A Rock Paper Scissors webapp with Python as the backend """
-from logic import computerChoice, play
+from flask import Flask, render_template, request, redirect, url_for
+from logic import computerChoice, play, options
+
+app = Flask(__name__)
+
+
+@app.route('/')  # route for the main page
+def index():
+    return render_template('index2.html', options=options)
+
+
+@app.route('/play', methods=['POST'])
+def playFrontend():
+    player = request.form['game-option']
+    computer = computerChoice()
+    outcome = play(computer, player)
+    return render_template('index2.html', options=options, outcome=outcome, player=player, computer=computer)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# print(play(computerChoice(), player))
